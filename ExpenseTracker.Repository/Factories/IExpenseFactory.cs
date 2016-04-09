54b5ef67fe6d82d069b9aef28542ddc7867ba03f
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-04-09 16:22:12 -0500

    Add interface so I can test expense controller

---
+++ ExpenseTracker.Repository/Factories/IExpenseFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseTracker.Repository.Entities;
+
+namespace ExpenseTracker.Repository.Factories
+{
+    public interface IExpenseFactory
+    {
+        DTO.Expense CreateExpense(Expense expense);
+        Expense CreateExpense(DTO.Expense expense);
+        IEnumerable<DTO.Expense> CreateExpenses(IEnumerable<Expense> expenses);
+        IEnumerable<Expense> CreateExpenses(IEnumerable<DTO.Expense> expenses);
+    }
+}
