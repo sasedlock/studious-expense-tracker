6db25acc71fb300ffb7e76b4d62acc927791b107
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-04-09 16:21:22 -0500

    Adding test class for expense controller

---
+++ ExpenseTracker.API.Tests/Controllers/ExpensesControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using ExpenseTracker.API.Controllers;
+using ExpenseTracker.Repository;
+using ExpenseTracker.Repository.Entities;
+using ExpenseTracker.Repository.Factories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace ExpenseTracker.API.Tests.Controllers
+{
+    [TestClass]
+    public class ExpensesControllerTests
+    {
+        private Mock<IExpenseTrackerRepository> _mockRepository;
+        private Mock<IExpenseFactory> _mockFactory;
+        private List<DTO.Expense> _expenseDtos;
+        private List<Expense> _expenseEntities;
+        private ExpensesController _controllerToTest;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
