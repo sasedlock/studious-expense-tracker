b63fceffdcc393c31a2ddeb01cc4b0cfddaf8048
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-05-02 18:47:21 -0500

    adding reference to Dapper and System.Configuration in Repository project, creating a new folder, interface and implementing class to get all expense groups

---
+++ ExpenseTracker.Repository/Dapper/ExpenseTrackerDapperRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using ExpenseTracker.Repository.Entities;
+
+namespace ExpenseTracker.Repository.Dapper
+{
+    public class ExpenseTrackerDapperRepository : IExpenseTrackerDapperRepository
+    {
+        internal IDbConnection Connection => new SqlConnection(ConfigurationManager.ConnectionStrings["ExpenseTrackerContext"].ConnectionString);
+
+        public IEnumerable<ExpenseGroup> GetAllExpenseGroups()
+        {
+            IEnumerable<ExpenseGroup> result;
+
+            using (IDbConnection connection = Connection)
+            {
+                connection.Open();
+                result = connection.Query<ExpenseGroup>("SELECT * FROM dbo.ExpenseGroup");
+            }
+
+            return result;
+        }
+    }
+}
