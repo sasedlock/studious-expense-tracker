f47576c4307dd10df11ca629f30e0981df4cea59
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-05-23 20:59:07 -0500

    Starting to break out collective repo into many repos, starting with creating a generic interface and implementing it for ExpenseGroupStatuses

---
+++ ExpenseTracker.Repository/ExpenseGroupStatusRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using ExpenseTracker.Repository.Entities;
+using ExpenseTracker.Repository.Interfaces;
+
+namespace ExpenseTracker.Repository
+{
+    public class ExpenseGroupStatusRepository : IExpenseTrackerGenericRepository<ExpenseGroupStatus>
+    {
+        private readonly IExpenseTrackerDbContext _context;
+
+        public ExpenseGroupStatusRepository(IExpenseTrackerDbContext context)
+        {
+            _context = context;
+        }
+
+        public ExpenseGroupStatus GetById(int id)
+        {
+            return _context.ExpenseGroupStatusses.FirstOrDefault(egs => egs.Id == id);
+        }
+
+        public IQueryable<ExpenseGroupStatus> GetAllAsQueryable()
+        {
+            return _context.ExpenseGroupStatusses;
+        }
+    }
+}
---
+++ ExpenseTracker.Repository/Interfaces/IExpenseTrackerGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+namespace ExpenseTracker.Repository.Interfaces
+{
+    public interface IExpenseTrackerGenericRepository<out T>
+    {
+        T GetById(int id);
+        IQueryable<T> GetAllAsQueryable();
+    }
+}
