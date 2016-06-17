5628e2fcf476c9c79a7c26364889258a245e1ae0
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-06-16 22:33:33 -0500

    adding web client project, and importing package for http client

---
+++ ExpenseTracker.WebClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExpenseTracker.WebClient.Startup))]
+namespace ExpenseTracker.WebClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
