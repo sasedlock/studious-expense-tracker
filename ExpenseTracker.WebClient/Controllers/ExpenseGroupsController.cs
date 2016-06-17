7ec15b0c7059d9bb06d8d1b33f08b86ae3cb1734
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-06-16 23:05:13 -0500

    adding controller for expense groups, as well as an index view for it

---
+++ ExpenseTracker.WebClient/Controllers/ExpenseGroupsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+using ExpenseTracker.DTO;
+using ExpenseTracker.WebClient.Helpers;
+using Newtonsoft.Json;
+
+namespace ExpenseTracker.WebClient.Controllers
+{
+    public class ExpenseGroupsController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var client = ExpenseTrackerHttpClient.GetClient();
+            HttpResponseMessage response = await client.GetAsync("api/expensegroups");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var model = JsonConvert.DeserializeObject<IEnumerable<ExpenseGroup>>(content);
+
+                return View(model);
+            }
+            else
+            {
+                return Content("An error occurred");
+            }
+        }
+    }
+}
