cd937a4fd4d2a8955db1e8b5e81f34508046ecc3
sean.sedlock <sean.sedlock@parivedasolutions.com>
2016-06-16 22:45:48 -0500

    Adding consolidated default http client

---
+++ ExpenseTracker.WebClient/Helpers/ExpenseTrackerHttpClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ExpenseTracker.WebClient.Helpers
+{
+    public static class ExpenseTrackerHttpClient
+    {
+        public static HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(ExpenseTrackerConstants.ExpenseTrackerAPI);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+    }
+}
